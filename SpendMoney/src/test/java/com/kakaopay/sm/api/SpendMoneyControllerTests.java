e721c92f96310eeb10a9e9bb21e721c0b9e18597
JJY <JJY@192.168.0.11>
2020-06-27 15:34:57 +0900

    단위테스트 추가2

--- SpendMoney/src/test/java/com/kakaopay/sm/api/SpendMoneyControllerTests.java
+++ SpendMoney/src/test/java/com/kakaopay/sm/api/SpendMoneyControllerTests.java
@@ -76,7 +76,49 @@ public class SpendMoneyControllerTests {
     }
 	
 	@Test
-	public void empty_send_Test() throws Exception {
+	public void empty_Header_Test() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("sendPsersonNum", "3");
+        map.put("sendTotalMoney", "30000");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<SpendMoneyApiResponse> response 
+        		= restTemplate.postForEntity("/api/v1/sendMoney", request, SpendMoneyApiResponse.class);
+        logger.info("sendMoney API: " + response);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getCode()).isEqualTo("0001");
+    }
+	
+	@Test
+	public void check_Header_Test() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        headers.set("X-USER-ID", "12345678910123456789dfajflk");
+        headers.set("X-ROOM-ID", "qwertyuiopasdfghjklzxcvbnmqwer");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("sendPsersonNum", "3");
+        map.put("sendTotalMoney", "30000");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<SpendMoneyApiResponse> response 
+        		= restTemplate.postForEntity("/api/v1/sendMoney", request, SpendMoneyApiResponse.class);
+        logger.info("sendMoney API: " + response);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getCode()).isEqualTo("0002");
+    }
+	
+	@Test
+	public void empty_Send_Test() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
         headers.set("X-USER-ID", "12345678910123456789101234567891");
@@ -96,7 +138,7 @@ public class SpendMoneyControllerTests {
     }
 	
 	@Test
-	public void empty_token_Test() throws Exception {
+	public void empty_Token_Test() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
         headers.set("X-USER-ID", "12345678910123456789101234567891");
@@ -113,6 +155,48 @@ public class SpendMoneyControllerTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().getCode()).isEqualTo("0005");
+    }
+	
+	@Test
+	public void noSearch_Test() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        headers.set("X-USER-ID", "12345678910123456789101234567891");
+        headers.set("X-ROOM-ID", "qwertyuiopasdfghjklzxcvbnmqwer");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", "XXX");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<SpendMoneyApiResponse> response 
+        		= restTemplate.postForEntity("/api/v1/searchMoney", request, SpendMoneyApiResponse.class);
+        logger.info("searchMoney API: " + response);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getCode()).isEqualTo("0006");
+    }	
+	
+	@Test
+	public void timeover_Search_Test() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        headers.set("X-USER-ID", "12345678910123456789101234567891");
+        headers.set("X-ROOM-ID", "qwertyuiopasdfghjklzxcvbnmqwer");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", "klw");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<SpendMoneyApiResponse> response 
+        		= restTemplate.postForEntity("/api/v1/searchMoney", request, SpendMoneyApiResponse.class);
+        logger.info("searchMoney API: " + response);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getCode()).isEqualTo("0007");
     }
 	
 	@Test
@@ -137,6 +221,27 @@ public class SpendMoneyControllerTests {
     }
 	
 	@Test
+	public void noReceive_Test() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        headers.set("X-USER-ID", "12345678910123456789101234567891");
+        headers.set("X-ROOM-ID", "qwertyuiopasdfghjklzxcvbnmqwer");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", "XXX");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<SpendMoneyApiResponse> response 
+        		= restTemplate.postForEntity("/api/v1/receiveMoney", request, SpendMoneyApiResponse.class);
+        logger.info("receiveMoney API: " + response);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getCode()).isEqualTo("0010");
+    }
+	
+	@Test
 	public void search_Test() throws Exception {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
@@ -155,48 +260,6 @@ public class SpendMoneyControllerTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody().getCode()).isEqualTo("0000");
-    }
-	
-	@Test
-	public void empty_header_Test() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-
-        Map<String, String> map = new HashMap<>();
-        map.put("sendPsersonNum", "3");
-        map.put("sendTotalMoney", "30000");
-
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
-
-        ResponseEntity<SpendMoneyApiResponse> response 
-        		= restTemplate.postForEntity("/api/v1/sendMoney", request, SpendMoneyApiResponse.class);
-        logger.info("sendMoney API: " + response);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getCode()).isEqualTo("0001");
-    }
-	
-	@Test
-	public void check_header_Test() throws Exception {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        headers.set("X-USER-ID", "12345678910123456789dfajflk");
-        headers.set("X-ROOM-ID", "qwertyuiopasdfghjklzxcvbnmqwer");
-
-        Map<String, String> map = new HashMap<>();
-        map.put("sendPsersonNum", "3");
-        map.put("sendTotalMoney", "30000");
-
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(map, headers);
-
-        ResponseEntity<SpendMoneyApiResponse> response 
-        		= restTemplate.postForEntity("/api/v1/sendMoney", request, SpendMoneyApiResponse.class);
-        logger.info("sendMoney API: " + response);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getCode()).isEqualTo("0002");
     }
     
 }
